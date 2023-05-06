b13eb796d86c8f59d9c3a74224a2f741dae43b57
Yavor Gornalov <116191485+yavor-gornalov@users.noreply.github.com>
2023-05-06 22:31:07 +0300

    02. Insert Data in Tables added

---
+++ 01. Data Definition And Data Types - Lab/02. Insert Data in Tables.sql
@@ -0,0 +1,10 @@
+use `gamebar`;
+
+INSERT INTO `employees`
+	(`id`, `first_name`, `last_name`)
+VALUES
+	(1, "Ivan", "Ivanov"),
+    (2, "Petar", "Petrov"),
+    (3, "Dimitar", "Dimitrov");
+
+select * from `employees`;
